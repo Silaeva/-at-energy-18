73d5efbc860112ce2316bf28fe1ba5fa718d425e
Anna <tkachenko481@mail.ru>
2019-12-20 17:20:56 +0300

    Добавляет JS в шапку и в слайдер

---
+++ source/js/mobile-menu.js
@@ -0,0 +1,33 @@
+var menu = document.querySelector(".main-nav");
+var toggle = menu.querySelector(".main-nav__toggle");
+var slider = document.querySelector(".slider");
+var btnBefore = slider.querySelector(".slider_button--past");
+var btnAfter = slider.querySelector(".slider_button--present");
+var slideBefore = slider.querySelector(".slider__image--before");
+var slideAfter = slider.querySelector(".slider__image--after");
+var bar = slider.querySelector(".slider__progress");
+
+if (menu) {
+  menu.classList.remove("main-nav--opened");
+  menu.classList.add("main-nav--closed");
+};
+
+toggle.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  menu.classList.toggle("main-nav--opened");
+  menu.classList.toggle("main-nav--closed");
+});
+
+btnBefore.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  slideAfter.classList.remove("slider__image--after-show");
+  slideBefore.classList.add("slider__image--before-show");
+  bar.classList.remove("slider__progress--after");
+});
+
+btnAfter.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  slideBefore.classList.remove("slider__image--before-show");
+  slideAfter.classList.add("slider__image--after-show");
+  bar.classList.add("slider__progress--after");
+});
