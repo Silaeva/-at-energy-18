3c32bcaa34662177d7c129f2fdfcee67009cbe17
Anna <tkachenko481@mail.ru>
2019-12-23 18:29:11 +0300

    Корректирует кроссбраузерность

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ gulp.task("refresh", function (done) {
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
   .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.optipng({optimizationLevel: 2}),
     imagemin.jpegtran({progressive: true}),
     imagemin.svgo()
   ]))
